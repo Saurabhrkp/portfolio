ab0859379c360b21a4c0e4a2b5196a4835d12209
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-01 11:44:26 +0530

    Added About and achievement

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,78 @@
 const About = () => {
   return (
-    <div className='container card bg-light shadow my-4'>
-      <div className='row'>
-        <div className='col'></div>
-        <div className='col'></div>
+    <>
+      <div style={{ height: '65px' }} id='about'></div>
+      <div className='container card shadow p-4'>
+        <h3 className='text-center'>About</h3>
+        <hr />
+        <div className='row'>
+          <div className='col p-4'>
+            <p className='text-muted m-0'>Date of Birth</p>
+            <p>02 Nov 1997</p>
+            <p className='text-muted m-0'>Permanent Address </p>
+            <p>
+              B-301 Shankheshwar Pearls,
+              <br />
+              Adharwadi Road,
+              <br />
+              Kalyan West 421301,
+              <br />
+              Maharashtra, India
+            </p>
+          </div>
+          <div className='col p-4'>
+            <p className='text-muted m-0'>Area Pin Code</p>
+            <p>421301</p>
+            <p className='text-muted m-0'>Marital Status</p>
+            <p>Single / Unmarried</p>
+            <p className='text-muted m-0'>Hometown</p>
+            <p>Prayagraj</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col px-4'>
+            <table className='table table-borderless'>
+              <thead>
+                <tr>
+                  <th scope='col'>Languages</th>
+                  <th scope='col'>Proficiency</th>
+                  <th scope='col'>Read</th>
+                  <th scope='col'>Write</th>
+                  <th scope='col'>Speak</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>English</td>
+                  <td>Proficient</td>
+                  <td>Yes</td>
+                  <td>Yes</td>
+                  <td>Yes</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>Hindi</td>
+                  <td>Proficient</td>
+                  <td>Yes</td>
+                  <td>Yes</td>
+                  <td>Yes</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <td>Marathi</td>
+                  <td>Beginner</td>
+                  <td>Yes</td>
+                  <td>Yes</td>
+                  <td>No</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,11 +1,40 @@
 const Achievements = () => {
   return (
-    <div className='container card bg-light shadow my-4'>
-      <div className='row'>
-        <div className='col'></div>
-        <div className='col'></div>
+    <>
+      <div style={{ height: '65px' }} id='achievements'></div>
+      <div className='container card shadow p-4'>
+        <div className='row'>
+          <div className='col px-4'>
+            <h3 className='text-center'>Achievements</h3>
+            <hr />
+            <h4>Certification</h4>
+            <h6>MongoDB Essentials - Understand the Basics of MongoDB</h6>
+            <p>
+              Certified By: Udemy <br />
+              Year of Completion: 2019
+            </p>
+            <h6>Workshop on Machine Learning</h6>
+            <p>
+              Certified By: College Campus <br />
+              Year of Completion: 2020
+            </p>
+            <h6>Hadoop Basic Course for Beginners to Professionals</h6>
+            <p>
+              Certified By: Edulearners Technologies and Udemy <br />
+              Year of Completion: 2019
+            </p>
+            <h6>Web Design with WordPress: Everything from Beginning to End</h6>
+            <p>
+              Certified By: Udemy <br />
+              Year of Completion: 2019
+            </p>
+            <h4>Event</h4>
+            <h6>Workshop on Basic JavaScript, Node.js and Express.js</h6>
+            <p>Got good feedbacks from participants.</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
