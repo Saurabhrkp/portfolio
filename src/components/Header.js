d1e71420fb0fcd3dcfe8d04a83770bb5efd8af5f
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-01 11:49:50 +0530

    Updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
       <div id='home' style={{ height: '70px' }}></div>
       <div className='container card shadow mt-5'>
         <div className='row justify-content-center p-4'>
-          <div className='col-sm-5 col-md-4 col-lg-3 text-center'>
+          <div className='col-sm-10 col-md-5 col-lg-4 text-center'>
             <img src={Photo} alt='Me' className='card-body img-fluid col' />
           </div>
-          <div className='col-sm-10 col-md-8 col-lg-9'>
+          <div className='col-sm-10 col-md-6 col-lg-8 my-auto text-center'>
             <h4 className='card-title pt-4'>Saurabh Rajesh Patel</h4>
             <div>
               <a href='mailto:sauarbhpatel230@gmail.com'>
